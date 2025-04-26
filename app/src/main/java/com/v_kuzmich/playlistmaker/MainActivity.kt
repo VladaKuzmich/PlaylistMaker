5ce475db8d114fae65c145f8c84d2d0b420e82f4
VladaKuzmich <vlada.gkuz@yandex.by>
2025-04-26 21:12:27 +0300

    [PLM:2] - sprint_8

--- app/src/main/java/com/v_kuzmich/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/v_kuzmich/playlistmaker/MainActivity.kt
@@ -1,14 +1,32 @@
 package com.v_kuzmich.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val mediaLibraryButton = findViewById<Button>(R.id.media_library_button)
+
+        searchButton.setOnClickListener {
+            val intent = Intent(this, SearchActivity::class.java)
+            startActivity(intent)
+        }
+
+        mediaLibraryButton.setOnClickListener {
+            val intent = Intent(this, MediaLibraryActivity::class.java)
+            startActivity(intent)
+        }
+
+        settingsButton.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
